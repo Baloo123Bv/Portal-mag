1cbf5101df9258139b5194596d833b8d3269c3ae
karimSATPRO <107763973+karimSATPRO@users.noreply.github.com>
2022-12-08 01:39:53 +0100

    Update 1Free-Stalker-Portals.sh
    Test

--- 1Free-Stalker-Portals.sh
+++ 1Free-Stalker-Portals.sh
@@ -3,5 +3,12 @@
 
 wget -O /home/stalker.conf "https://raw.githubusercontent.com/karimSATPRO/Portal-100mag/main/stalker.conf"
 
+echo ""
+cd ..
+sync
+echo "############ INSTALLATION COMPLETED ########"
+echo "############ RESTARTING... #################" 
+init 4
+sleep 2
+init 3
 exit 0
-
